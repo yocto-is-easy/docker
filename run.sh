24d15fb9a288d1a49478330402432c44e902d113
Mykhailo Didur <mikhaildidur2003@gmail.com>
2022-08-30 22:29:20 +0300

    Run poky env setup script on startup

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 
 # run the container with current host user
-docker run -ti --volume=${PWD}/workdir:/home/build/workdir --user "$(id -u):$(id -g)" yocto-docker-img
+docker run -ti \
+    --volume=${PWD}/workdir:/home/build/workdir \
+    --user "$(id -u):$(id -g)" yocto-docker-img \
+    bash -c "cd workdir && source poky/oe-init-build-env && bash"
